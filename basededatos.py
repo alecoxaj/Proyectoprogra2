0f14011ad4c5f33cb9e2b5792fc044d668ae4b06
Alejandro <alejandrocoxaj94@gmail.com>
2025-10-18 18:01:00 -0600

    Alejandro hizo creación de la tabla "usuarios" con su id con llave primaria y autoincrementable, nombre con requerimiento de que no quede vacío, así como en usuario y contraseña.

--- basededatos.py
+++ basededatos.py
@@ -6,3 +6,13 @@ def crear_base_datos():
 
     cursor.execute("PRAGMA foreign_keys = ON")
 
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT NOT NULL,
+            usuario TEXT UNIQUE NOT NULL,
+            contraseña TEXT NOT NULL,
+            rol TEXT CHECK(rol IN ('admin', 'usuario')) NOT NULL
+        )
+        """)
+
