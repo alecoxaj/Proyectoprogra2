e20d9afbf67724669f1b1e994ab46b2c5295ca1f
Marvin7715 <vasquezmarvin588@gmail.com>
2025-10-18 19:03:10 -0600

    Se implemento la funcion verificar_login() que consulta rol en SQLite.

--- login_app.py
+++ login_app.py
@@ -3,3 +3,17 @@ from tkinter import messagebox
 import sqlite3
 
 DB_PATH = "espacio_creativo.db"
+
+def verificar_login(usuario, contraseña):
+    conexion = sqlite3.connect(DB_PATH)
+    cursor = conexion.cursor()
+    cursor.execute("SELECT rol FROM usuarios WHERE usuario=? AND contraseña=?", (usuario, contraseña))
+    resultado = cursor.fetchone()
+    conexion.close()
+    if resultado:
+        print(f"Usuario '{usuario}' inició sesión como {resultado[0]}.")
+        return resultado[0]
+    else:
+        print(f"Intento fallido de inicio de sesión con usuario '{usuario}'.")
+        return None
+
