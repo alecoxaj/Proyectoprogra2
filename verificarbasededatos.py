d4aeea6023660a6131d5e5f737b0e0cf73b1d6c5
Alejandro <alejandrocoxaj94@gmail.com>
2025-10-21 09:33:05 -0600

    Alejandro hizo el apartado de mostrar las tablas que tengamos en la base existentes.

--- verificarbasededatos.py
+++ verificarbasededatos.py
@@ -7,3 +7,12 @@ def verificar_conexion():
 
         print("Conexión establecida con la base de datos.")
 
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tablas = cursor.fetchall()
+
+        print("\nTablas encontradas:")
+        for t in tablas:
+            print(" -", t[0])
+
+
+
