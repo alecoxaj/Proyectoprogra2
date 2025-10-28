661b8570ccebe44219babb17d3b80440d5e0343b
Marvin7715 <vasquezmarvin588@gmail.com>
2025-10-28 00:02:14 -0600

    Seguidamente se sigue trabajando en el mismo y se agrega formulario (nombre, descripción, precio).

--- crud_servicios.py
+++ crud_servicios.py
@@ -12,3 +12,15 @@ def ventana_servicios():
     ventana.title("Gestión de Servicios - Espacio Creativo")
     ventana.geometry("650x420")
     ventana.config(bg="#f0f0f0")
+
+    tk.Label(ventana, text="Nombre:").grid(row=0, column=0, padx=5, pady=5, sticky="e")
+    entry_nombre = tk.Entry(ventana, width=45)
+    entry_nombre.grid(row=0, column=1, padx=5, pady=5)
+
+    tk.Label(ventana, text="Descripción:").grid(row=1, column=0, padx=5, pady=5, sticky="e")
+    entry_descripcion = tk.Entry(ventana, width=45)
+    entry_descripcion.grid(row=1, column=1, padx=5, pady=5)
+
+    tk.Label(ventana, text="Precio:").grid(row=2, column=0, padx=5, pady=5, sticky="e")
+    entry_precio = tk.Entry(ventana, width=20)
+    entry_precio.grid(row=2, column=1, padx=5, pady=5, sticky="w")
